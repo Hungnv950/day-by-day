75501ebde4601ec3ba82f7aeb873af31019d1223
hungnv950 <hungnv950@gmail.com>
2018-01-29 17:45:44 +0700

    array_hash.rb

---
+++ ruby/array_hash.rb
@@ -0,0 +1,14 @@
+nums = [1, 2, 3, 4]
+ 
+puts "There are #{nums.size} items in the array"
+ 
+nums.each do |num|
+    puts num
+end
+ 
+ 
+domains = { :de => "Germany", :sk => "Slovakia",
+            :us => "United States", :no => "Norway" }
+ 
+puts domains.keys
+puts domains.values
